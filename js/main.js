c81e2b6d81be0f1756109feda23bb26f6751c38f
Judson Mitchell <judsonmitchell@gmail.com>
2013-04-23 07:25:03 -0500

    Begin favorites

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+function updateFavoritesList() {
+
+    if (localStorage.length > 0) {
+        var favList = '';
+        for (var i = 0; i < localStorage.length; i++) {
+            var key = localStorage.key(i);
+            var value = localStorage.getItem(key);
+            favList += '<li><a class="fav-link" href="#" data-id="' + key + '">' + value + '</a></li>';
+            console.log(key + ' ' + value);
+        }
+
+        $('.dropdown-menu').html(favList);
+    }
+}
+
 //Handle history
 $(function () {
     var History = window.History;
@@ -6,12 +21,14 @@ $(function () {
         // set initial state to first page that was loaded
         var t = State.url.queryStringToJSON();
         History.pushState({type: t.view, id: t.target}, $('title').text(), State.urlPath);
+        updateFavoritesList();
     } else {
         return false;
     }
 
     History.Adapter.bind(window, 'statechange', function () {
         updateContent(History.getState());
+        updateFavoritesList();
     });
 });
 
@@ -49,15 +66,25 @@ var updateContent = function(State) {
         break;
     case 'law':
         laws = jlinq.from(myData).equals('id', target).select();
+        //check to see if this law has been favorited
+        var fav;
+        if (localStorage.getItem(target)){
+            fav = '<a href="#" class="favorite" data-state="saved" data-id="' + target +
+            '" title="Favorite This"><i class="icon-ok-sign"></i></a>';
+        }
+        else {
+            fav = '<a href="#" class="favorite" data-state="unsaved" data-id="' + target +
+            '" title="Favorite This"><i class="icon-ok-circle"></i></a>';
+        }
         $('title').text(laws[0].description + ' ' + laws[0].title);
-        $('.well').html('<h3>' + laws[0].description + '</h3>' + laws[0].law_text);
-        $(document).scrollTop(pos);
+        $('.well').html('<h3><span class="lawTitle">' + laws[0].description + '</span>' + fav + '</h3>' + laws[0].law_text);
+        $(document).scrollTop(0);
         break;
     case 'search':
         laws = jlinq.from(myData).contains('law_text', target).select();
-        items = '<div class="continer">';
+        items = '<div class="container">';
         $.each(laws, function (key, value) {
-            var snippet = getExcerpt(value.law_text, target, 5); 
+            var snippet = getExcerpt(value.law_text, target, 5);
             items += '<h4><a class="law-link" href="#" data-id="' + value.id +
             '">' + value.description + ' ' + value.title + '</a></h4>' +
             '<p>...' + snippet + '...</p>';
@@ -97,6 +124,32 @@ $(document).ready(function () {
         History.pushState({type: 'search', id: target, pos: scroll}, target, '?target=' + target + '&view=search');
     });
 
+    $('.main').on('click', 'a.favorite', function (event) {
+        event.preventDefault();
+        var target = $(this).attr('data-id');
+        var saveState = $(this).attr('data-state');
+        if (saveState === 'unsaved') {
+            var title = $(this).prev().html();
+            localStorage.setItem(target, title);
+
+            $('.alert').html('Saved to favorites.').show();
+            $('.favorite').html('<i class="icon-ok-sign"></i>');
+            $(this).attr('data-state', 'saved');
+        }
+        else {
+            localStorage.removeItem(target);
+            $(this).attr('data-state', 'unsaved');
+            $('.alert').html('Removed from favorites.').show();
+            $('.favorite').html('<i class="icon-ok-circle"></i>');
+        }
+    });
+
+    $('.navbar-headnav').on('click', 'a.fav-link', function (event) {
+        event.preventDefault();
+        var target = $(this).attr('data-id');
+        var scroll = $(document).scrollTop();
+        History.pushState({type: 'law', id: target, pos: scroll}, target, '?target=' + target + '&view=law');
+    });
     //Handle swipes
    // $('.main').wipetouch({
    //     wipeLeft: function (result) {History.go(1); },

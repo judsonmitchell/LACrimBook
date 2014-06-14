b7926ce7b924e03fa8bed366f427ee4f8edf1023
Judson Mitchell <judsonmitchell@gmail.com>
2014-06-14 11:59:20 -0500

    Answer: not really regex either

--- js/main.js
+++ js/main.js
@@ -92,9 +92,9 @@ var updateContent = function(State,callback) {
         console.log('update content done');
         break;
     case 'search':
-        //var regex = new RegExp('\\b' + target + '\\b');
-        //laws = jlinq.from(myData).match('description', regex).or().match('law_text', regex).or().match('title', regex).select();
-        laws = jlinq.from(myData).contains('description', target).or().contains('law_text', target).or().contains('title', target).select();
+        var regex = new RegExp('\\b' + target + '\\b');
+        laws = jlinq.from(myData).match('description', regex).or().match('law_text', regex).or().match('title', regex).select();
+        //laws = jlinq.from(myData).contains('description', target).or().contains('law_text', target).or().contains('title', target).select();
         items = '<div class="list-group">';
         if (!laws.length){
             items += '<a class="list-group-item">No results found.</a>';
